279a1c344861e67d878305d54094df98930dd934
ARNAB DAS <raj713335@gmail.com>
2020-08-23 23:29:47 +0530

    initial commit

---
+++ PROJ1/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
