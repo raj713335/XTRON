19d219bc12dbf3b00f1ce7c7403880f9fdda46d8
ARNAB DAS <raj713335@gmail.com>
2020-08-25 08:13:23 +0530

    Added Functionality to upload Images Complete

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 try:
     from flask import Flask
     from flask import redirect,url_for,request,render_template
-    from flask_wtf.file import FileField
+    from flask_wtf.file import FileField 
     from wtforms import SubmitField
     from flask_wtf import Form
     import sqlite3
+    from datetime import datetime
     print("All Modules Loaded properly ....")
 except:
     print("Some Modules are missing ....")
@@ -21,11 +22,11 @@ class Upload_Chest_Xray(Form):
     submit = SubmitField("submit")
 
 
-def patient_database(name,data):
+def patient_database(name,filename,data):
     conn=sqlite3.connect("PATIENT_DB")
     cursor=conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS demo_table (name TEXT,data BLOB)""")
-    cursor.execute("""INSERT INTO demo_table (name,data) VALUES (?,?) """,(name,data))
+    cursor.execute("""CREATE TABLE IF NOT EXISTS PATIENT_TABLE (name TEXT,filename TEXT,data BLOB)""")
+    cursor.execute("""INSERT INTO PATIENT_TABLE (name,filename,data) VALUES (?,?,?) """,(name,filename,data))
     conn.commit()
     cursor.close()
     conn.close()
@@ -37,26 +38,28 @@ def patient_database(name,data):
 # class PatientData(db.Model):
 #     p_id = db.Column(db.Integer, primary_key=True)
 #     name = db.Column(db.String(30), nullable=False)
+#     age = db.Column(db.Integer(30), nullable=False)
 #     race = db.Column(db.String(30), nullable=False)
 #     sex = db.Column(db.String(10), nullable=False)
-#     diastolic_bp = db.Column(db.String(10), nullable=False)
-#     red_blood_cells = db.Column(db.String(30), nullable=False)
-#     sedimentation_rate = db.Column(db.String(30), nullable=False)
-#     serum_albumin = db.Column(db.String(30), nullable=False)
-#     serum_cholestrol = db.Column(db.String(30), nullable=False)
-#     serum_iron = db.Column(db.String(30), nullable=False)
-#     serum_magnessium = db.Column(db.String(30), nullable=False)
-#     serum_protein = db.Column(db.String(30), nullable=False)
-#     systolic_bp = db.Column(db.String(30), nullable=False)
-#     tibc = db.Column(db.String(30), nullable=False)
-#     ts = db.Column(db.String(30), nullable=False)
-#     white_blood_cells = db.Column(db.String(30), nullable=False)
-#     bmi = db.Column(db.String(30), nullable=False)
-#     pulse_pressure = db.Column(db.String(30), nullable=False)
-
-
-
-
+#     diastolic_bp = db.Column(db.Integer(10), nullable=False)
+#     red_blood_cells = db.Column(db.Integer(30), nullable=False)
+#     sedimantation_rate = db.Column(db.Integer(30), nullable=False)
+#     serum_albumin = db.Column(db.Integer(30), nullable=False)
+#     serum_cholestrol = db.Column(db.Integer(30), nullable=False)
+#     serum_iron = db.Column(db.Integer(30), nullable=False)
+#     serum_magnesium = db.Column(db.Integer(30), nullable=False)
+#     serum_protein = db.Column(db.Integer(30), nullable=False)
+#     systolic_bp = db.Column(db.Integer(30), nullable=False)
+#     tibc = db.Column(db.Integer(30), nullable=False)
+#     ts = db.Column(db.Integer(30), nullable=False)
+#     white_blood_cells = db.Integer(db.String(30), nullable=False)
+#     bmi = db.Column(db.Integer(30), nullable=False)
+#     pulse_pressure = db.Column(db.Integer(30), nullable=False)
+#     date_time=db.Column(db.)
+#
+#
+#
+#
 #    def __repr__(self):
 #        return 'Patient Data ' + str(self.id)
 
@@ -72,8 +75,10 @@ def form():
     form = Upload_Chest_Xray()
     if request.method=="POST":
         if form.validate_on_submit():
+            name=form.file.data
             file_name=form.file.data
-            patient_database(name=file_name.filename,data=file_name.read())
+
+            patient_database(name=name.filename,filename=file_name.filename,data=file_name.read())
             print("FILE : {}".format((file_name.filename)))
             return render_template('form.html',form=form)
     return render_template('form.html',form=form)
